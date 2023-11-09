c63c283607d5dfa919033d35d81fa1dfbb82faa0
Keidson Roby <keidsondesigner@gmail.com>
2023-11-09 11:06:34 -0400

    Criado tabela vagas JobEntity

--- src/main/java/br/com/keidsonroby/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/keidsonroby/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,8 +25,11 @@ public class JobEntity {
   // Foreign Key (FK); 
   // Muitos Jobs para uma company;
   @ManyToOne
-  @JoinColumn(name = "company_id")
+  @JoinColumn(name = "company_id", insertable = false, updatable = false)
   private CompanyEntity companyEntity;
+
+  @Column(name = "company_id")
+  private UUID companyId;
 
   private String description;
   private String level;
