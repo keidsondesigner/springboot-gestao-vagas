ba36d03c6a6430aacfeb722884dc109f7b464caa
Keidson Roby <keidsondesigner@gmail.com>
2024-04-21 14:20:40 -0400

    feat: Sprin Security criado encriptação de senhas, Implentado encriptação de senha no CreateCompanyUseCase

--- src/main/java/br/com/keidsonroby/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/keidsonroby/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package br.com.keidsonroby.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.keidsonroby.gestao_vagas.exceptions.UserFoundException;
@@ -12,6 +13,9 @@ public class CreateCompanyUseCase {
 
   @Autowired
   private CompanyRepository companyRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
   
   public CompanyEntity execute(CompanyEntity companyEntity) {
 
@@ -19,6 +23,9 @@ public class CreateCompanyUseCase {
       .ifPresent((user) -> {
         throw new UserFoundException();
       });
+
+      var password = passwordEncoder.encode(companyEntity.getPassword());
+      companyEntity.setPassword(password);
 
     return this.companyRepository.save(companyEntity);
   }
