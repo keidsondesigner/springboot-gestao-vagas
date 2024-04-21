35d2e30ac238d5cfccbaa7b1729f8f7175c4dbab
Keidson Roby <keidsondesigner@gmail.com>
2024-04-20 23:30:00 -0400

    chore: CandidateEntity atualização na validação do campo "name"

--- src/main/java/br/com/keidsonroby/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/br/com/keidsonroby/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -11,6 +11,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -19,10 +21,13 @@ import lombok.Data;
 public class CandidateEntity {
 
   @Id // id vai ser a chave primária;
-  @GeneratedValue(strategy = GenerationType.UUID) // cria o id único automaticamente;
+  @GeneratedValue(strategy = GenerationType.UUID) // cria o id único automaticamente ou  GenerationType.AUTO;
   private UUID id;
 
   // @Column(name = "nome") // para renomear o nome da minha coluna;
+  @NotNull(message = "O campo [name], não foi informado.")
+  @NotBlank(message = "O campo [name], não poder conter caractérs em branco.")
+  @Length(min = 5, max = 10, message = "O campo [name], deve possuir no minimo ${min} caracteres e no máximo ${max}")
   private String name;
 
   @Pattern(regexp = "\\S+", message =  "O campo [username], não deve conter espaços, ou use underline * _ * em espaços.")
