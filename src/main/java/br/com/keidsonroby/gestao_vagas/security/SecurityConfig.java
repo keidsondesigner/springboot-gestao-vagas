6846570f90dea38da2229d3eb771fe3fb6c4e2b7
Keidson Roby <keidsondesigner@gmail.com>
2024-04-21 13:56:19 -0400

    chore: Spring Security - Configuração de Rostas públicas e Rotas privadas

--- src/main/java/br/com/keidsonroby/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/keidsonroby/gestao_vagas/security/SecurityConfig.java
@@ -10,7 +10,14 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrfCustomizer -> csrfCustomizer.disable());
+    http.csrf(csrfCustomizer -> csrfCustomizer.disable())
+        .authorizeHttpRequests((auth) -> {
+          // Rotas públicas;
+          auth.requestMatchers("/candidate/").permitAll()
+              .requestMatchers("/company/").permitAll();
+          // E qualque outra, serão Rotas privadas e precisam de autenticação;
+          auth.anyRequest().authenticated();
+        });
     return http.build();
   }
 
