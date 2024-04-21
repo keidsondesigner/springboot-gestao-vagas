de25625076b0920340fbaec40607c8bb30c59c7c
Keidson Roby <keidsondesigner@gmail.com>
2024-04-21 13:43:00 -0400

    feat: Instalação e Configuração do Spring Security

---
+++ src/main/java/br/com/keidsonroby/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package br.com.keidsonroby.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrfCustomizer -> csrfCustomizer.disable());
+    return http.build();
+  }
+
+}
