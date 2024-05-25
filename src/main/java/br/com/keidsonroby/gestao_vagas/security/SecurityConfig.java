12e1d28632d5fc4d3b3e979c508371c3b05756f0
Keidson Roby <keidsondesigner@gmail.com>
2024-05-25 09:53:19 -0400

    feat: :sparkles: SecurityConfig:  Liberando rota "/auth/company"  como pública

--- src/main/java/br/com/keidsonroby/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/keidsonroby/gestao_vagas/security/SecurityConfig.java
@@ -12,17 +12,19 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrfCustomizer -> csrfCustomizer.disable())
+    http.csrf(csrfCustomizer -> csrfCustomizer.disable()) // Desabilitar Spring Security para desabilitar o CSRF
         .authorizeHttpRequests((auth) -> {
           // Rotas públicas;
           auth.requestMatchers("/candidate/").permitAll()
-              .requestMatchers("/company/").permitAll();
+              .requestMatchers("/company/").permitAll()
+              .requestMatchers("/auth/company").permitAll();
           // E qualque outra, serão Rotas privadas e precisam de autenticação;
           auth.anyRequest().authenticated();
         });
     return http.build();
   }
 
+  // CRIPTOGRAFIA DA SENHA
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
