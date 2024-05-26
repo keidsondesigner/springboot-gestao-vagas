02f79efa01cf05a3977ddfecba70206d7d4d4bea
Keidson Roby <keidsondesigner@gmail.com>
2024-05-26 09:28:17 -0400

    feat: :sparkles: Criado AuthCandidateResponseDTO

---
+++ src/main/java/br/com/keidsonroby/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.keidsonroby.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder // Para criar a instancia
+@NoArgsConstructor // Para criar um construtor vazio
+@AllArgsConstructor // Para criar um construtor com todos os atributos
+public class AuthCandidateResponseDTO {
+
+  private String acess_token;
+
+}
